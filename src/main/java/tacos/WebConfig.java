9efd234e452177e5592412c3b0858e33c44e54a2
wjk <wddamw@gmail.com>
2020-04-22 15:31:09 +0800

    使用WebMvcConfigurer视图控制器重定向默认/请求

---
+++ src/main/java/tacos/WebConfig.java
@@ -0,0 +1,20 @@
+package tacos;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @program: TacoCloud
+ * @description: WebConfig配置类
+ * @author: wjk
+ * @create: 2020-04-22 15:25
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}

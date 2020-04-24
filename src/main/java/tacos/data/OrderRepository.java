b19145a4071cbddeeb32182af25098b5c5c13b7c
wjk <wddamw@gmail.com>
2020-04-24 15:04:38 +0800

    使用Spring Data的CrudRepository进行repository的拓展，自动完成CRUD相关操作

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -1,8 +1,19 @@
 package tacos.data;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import tacos.Order;
 
-public interface OrderRepository {
+import java.util.Date;
+import java.util.List;
 
-    Order save(Order order);
+public interface OrderRepository extends CrudRepository<Order, Long> {
+//    List<Order> findByDeliveryZip(String deliveryZip);
+//
+//    List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+//
+//    List<Order> findByDeliveryToAndDeliveryCityAllIgnoresCase(String deliveryTo, String deliveryCity);
+//
+//    @Query("Order o where o.deliveryCity = 'Seattle'")
+//    List<Order> readOrdersDeliveredInSeattle();
 }
